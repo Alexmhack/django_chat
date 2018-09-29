add7150dff7295e27e33dfe9fa1c7a3017e98c70
alexHooper <alexmohack@gmail.com>
2018-09-29 23:33:34 +0530

    add rest framework auth classes

--- chatire/settings.py
+++ chatire/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# django rest frameworks authentication classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
