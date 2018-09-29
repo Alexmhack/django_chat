4f108fda61c91b8e60cd08d7fbbe6d6ae9f1ed69
alexHooper <alexmohack@gmail.com>
2018-09-30 00:39:21 +0530

    add routes for chat and auts

--- chatire-frontend/src/router/index.js
+++ chatire-frontend/src/router/index.js
@@ -9,9 +9,14 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/chats',
+      name: 'Chat',
+      component: Chat
+    },
+
+    {
+  ]	  path: '/auth',
+  	  name: 'UserAuth',
+  	  component: UserAuth
     }
-  ]
 })
