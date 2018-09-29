c4108fc4ed98c84d5cc60988b1d75333684d49b9
alexHooper <alexmohack@gmail.com>
2018-09-30 00:36:41 +0530

    import chat and userauth in index file

--- chatire-frontend/src/router/index.js
+++ chatire-frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld';
+import Chat from '@/components/Chat';
+import UserAuth from '@/components/UserAuth';
 
 Vue.use(Router)
 
