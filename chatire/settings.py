2057cd5d1b8d6584e14d7a43c88d666673c42c13
alexHooper <alexmohack@gmail.com>
2018-09-30 10:32:40 +0530

    add comments

--- chatire/settings.py
+++ chatire/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # packages
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
