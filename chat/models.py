21be147e41e7d604bcf23efa73a013411b39efac
alexHooper <alexmohack@gmail.com>
2018-09-30 11:32:58 +0530

    deserialize user instance to JSON

--- chat/models.py
+++ chat/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 from uuid import uuid4
+
+User = get_user_model()
+
+def deserialize_user(user):
+	"""Deserialize user instance to JSON"""
+	return {
+		'id': user.id, 'username': user.username, 'email': user.email,
+		'first_name': user.first_name, 'last_name': user.last_name
+	}
