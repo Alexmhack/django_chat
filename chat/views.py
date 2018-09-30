b058965c5930395a7820a05a7a99dbc826f3307f
alexHooper <alexmohack@gmail.com>
2018-09-30 12:14:28 +0530

    import the packages in views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .models import (
+	deserialize_user, ChatSession, ChatSessionMessage, ChatSessionMember
+)
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
