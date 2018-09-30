2b58f07893a2cd388b5ab5c7b8490f52752cb29b
alexHooper <alexmohack@gmail.com>
2018-09-30 11:36:30 +0530

    Abstract model to track the creation/update date for a model

--- chat/models.py
+++ chat/models.py
@@ -11,3 +11,13 @@ def deserialize_user(user):
 		'id': user.id, 'username': user.username, 'email': user.email,
 		'first_name': user.first_name, 'last_name': user.last_name
 	}
+
+
+class TrackableDateModel(models.Model):
+	"""Abstract model to track the creation/update date for a model"""
+
+	created_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
