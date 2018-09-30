04ebc7b5dd7128989592f112d5fe62a0b807c0d7
alexHooper <alexmohack@gmail.com>
2018-09-30 12:18:41 +0530

    define post method for chat session view

--- chat/views.py
+++ chat/views.py
@@ -13,3 +13,14 @@ class ChatSessionView(APIView):
 	"""Manage Chat Session"""
 
 	permission_classes = (permissions.IsAuthenticated,)
+
+	def post(self, request, *args, **kwargs):
+		"""create a new chat session"""
+		user = request.user
+
+		chat_session = ChatSession.objects.create(owner=user)
+
+		return Response({
+			'status': 'SUCCESS', 'uri': chat_session.uri,
+			'message': 'New chat session created'
+		})
