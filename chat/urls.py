c29413678800d3f2de40f9bc62f6df5d03e85203
alexHooper <alexmohack@gmail.com>
2018-10-01 23:42:49 +0530

    correct error in urlpattern

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from .views import (
 	ChatSessionView, ChatSessionMessageView
 )
 
-urlpatterns += [
+urlpatterns = [
 	path('chats/', ChatSessionView.as_view()),
 	path('chats/<uri>/', ChatSessionView.as_view()),
 	path('chats/<uri>/messages/', ChatSessionMessageView.as_view()),
