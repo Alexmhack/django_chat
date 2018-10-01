73eeb6da7efd54bb565a9a9010b6a82e07a377c9
alexHooper <alexmohack@gmail.com>
2018-10-01 21:03:55 +0530

    add urls for chat

---
+++ chat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+	ChatSessionView, ChatSessionMessageView
+)
+
+urlpatterns += [
+	path('chats/', ChatSessionView.as_view()),
+	path('chats/<uri>/', ChatSessionView.as_view()),
+	path('chats/<uri>/messages/', ChatSessionMessageView.as_view()),
+]
